2f05665418753fb29c44f0900dcf219ec62effca
aidanvyas <68206970+aidanvyas@users.noreply.github.com>
2024-04-23 07:20:19 +0000

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return 'Hello from Github hopefully!'
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
